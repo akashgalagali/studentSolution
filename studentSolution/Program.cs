26ab7147a9b991f933a986052a755e3e0901be88
akashgalagali <akashgalagali4@gmail.com>
2021-10-11 12:11:58 +0530

    Add project files.

---
+++ studentSolution/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace studentSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
